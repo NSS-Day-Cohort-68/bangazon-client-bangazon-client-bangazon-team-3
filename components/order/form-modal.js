80cceefb9d974c85b61dea1801d8dba48dec6789
Gavin Kashmark <gkashmark@gmail.com>
2024-04-01 11:30:36 -0500

    BUG FIX users can now complete orders

--- components/order/form-modal.js
+++ components/order/form-modal.js
@@ -1,20 +1,20 @@
 import { useState } from "react"
 import Modal from "../modal"
 
-export default function CompleteFormModal({ showModal, setShowModal, paymentTypes, completeOrder }) {
+export default function CompleteFormModal({ showModal, setShowModal, paymentTypes, completeOrder, cart }) {
   const [selectedPayment, setSelectedPayment] = useState(0)
   return (
     <Modal showModal={showModal} setShowModal={setShowModal} title="Complete Order">
       <div className="select">
-        <select value={selectedPayment} onChange={(event) => setSelectedPayment(event.target.value)}>
+        <select value={selectedPayment} onChange={(event) => setSelectedPayment(parseInt(event.target.value))}>
           <option>Select a payment type to complete your order</option>
           {
-            paymentTypes.map(pt => <option key={pt.id} value={pt.id}>{pt.merchant_name} {pt.obscured_num}</option>)
+            paymentTypes.map(pt => <option key={pt.id} value={pt.id}>{pt.merchant_name} {pt.account_number}</option>)
           }
         </select>
       </div>
       <>
-        <button className="button is-success" onClick={() => completeOrder(selectedPayment)}>Complete Order</button>
+      <button className="button is-success" onClick={() => completeOrder(cart.id, selectedPayment)}>Complete Order</button>
         <button className="button" onClick={() => setShowModal(false)}>Cancel</button>
       </>
     </Modal>

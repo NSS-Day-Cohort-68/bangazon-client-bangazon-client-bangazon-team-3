fb05a2aea069f2b393e212f64a359d221abc1658
Eric Heidel <ericleeheidel@yahoo.com>
2024-04-04 16:05:23 -0500

    added functionality to not show stores wihtout products, need test

--- pages/stores/index.js
+++ pages/stores/index.js
@@ -1,15 +1,14 @@
-import { useEffect, useState } from 'react'
-import Layout from '../../components/layout'
-import Navbar from '../../components/navbar'
-import { StoreCard } from '../../components/store/card'
-import { getStores } from '../../data/stores'
-
+import { useEffect, useState } from "react"
+import Layout from "../../components/layout"
+import Navbar from "../../components/navbar"
+import { StoreCard } from "../../components/store/card"
+import { getStores } from "../../data/stores"
 
 export default function Stores() {
   const [stores, setStores] = useState([])
 
   useEffect(() => {
-    getStores().then(data => {
+    getStores().then((data) => {
       if (data) {
         setStores(data)
       }
@@ -20,11 +19,12 @@ export default function Stores() {
     <>
       <h1 className="title">Stores</h1>
       <div className="columns is-multiline">
-      {
-        stores.map(store => (
-          <StoreCard store={store} key={store.id} />
-        ))
-      }
+        {stores.map(
+          (store) =>
+            store.products.length !== 0 && (
+              <StoreCard store={store} key={store.id} />
+            )
+        )}
       </div>
     </>
   )

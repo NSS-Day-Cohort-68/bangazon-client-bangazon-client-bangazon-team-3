bc2527deacc754e3b67848b2f634a2d2aa6d84fe
Eric Heidel <ericleeheidel@yahoo.com>
2024-04-05 10:54:33 -0500

    state clearing on logout

--- pages/stores/new.js
+++ pages/stores/new.js
@@ -1,10 +1,10 @@
-import { useRouter } from 'next/router'
-import { useRef } from 'react'
-import Layout from '../../components/layout'
-import Navbar from '../../components/navbar'
-import { addStore } from '../../data/stores'
-import { useAppContext } from '../../context/state'
-import StoreForm from '../../components/store/form'
+import { useRouter } from "next/router"
+import { useRef } from "react"
+import Layout from "../../components/layout"
+import Navbar from "../../components/navbar"
+import { addStore } from "../../data/stores"
+import { useAppContext } from "../../context/state"
+import StoreForm from "../../components/store/form"
 
 export default function NewStore() {
   const { setProfile, profile } = useAppContext()
@@ -16,19 +16,28 @@ export default function NewStore() {
   const saveStore = () => {
     addStore({
       name: nameEl.current.value,
-      description: descriptionEl.current.value
+      description: descriptionEl.current.value,
     }).then((res) => {
       setProfile({
         ...profile,
-        store: res
+        store: res,
       })
       router.push(`/stores/${res.id}`)
     })
   }
 
   return (
-    <StoreForm nameEl={nameEl} descriptionEl={descriptionEl} saveEvent={saveStore} router={router} title="Create your store">
-      <p>Give your new store a name and description. Then add products on the next page</p>
+    <StoreForm
+      nameEl={nameEl}
+      descriptionEl={descriptionEl}
+      saveEvent={saveStore}
+      router={router}
+      title="Create your store"
+    >
+      <p>
+        Give your new store a name and description. Then add products on the
+        next page
+      </p>
     </StoreForm>
   )
 }

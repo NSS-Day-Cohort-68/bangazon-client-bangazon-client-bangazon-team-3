becd2d9c06b870e964dc992423056177a704ddf8
Aubry Freeman <aubryfreeman23@gmail.com>
2024-04-09 14:26:56 -0500

    Added functionality to filtering w a category header

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,57 +1,104 @@
-import { useEffect, useState } from 'react'
-import Filter from '../../components/filter'
-import Layout from '../../components/layout'
-import Navbar from '../../components/navbar'
-import { ProductCard } from '../../components/product/card'
-import { getProducts } from '../../data/products'
+import { useEffect, useState } from "react";
+import Filter from "../../components/filter";
+import Layout from "../../components/layout";
+import Navbar from "../../components/navbar";
+import { ProductCard } from "../../components/product/card";
+import { getCategories, getProducts } from "../../data/products";
 
 export default function Products() {
-  const [products, setProducts] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [loadingMessage, setLoadingMessage] = useState("Loading products...")
-  const [locations, setLocations] = useState([])
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingMessage, setLoadingMessage] = useState("Loading products...");
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    getProducts().then(data => {
+    getCategories()
+    .then((data) => {
       if (data) {
-
-        const locationData = [...new Set(data.map(product => product.location))]
-        const locationObjects = locationData.map(location => ({
-          id: location,
-          name: location
-        }))
-
-        setProducts(data)
-        setIsLoading(false)
-        setLocations(locationObjects)
+        setCategories(data)
       }
     })
-    .catch(err => {
-      setLoadingMessage(`Unable to retrieve products. Status code ${err.message} on response.`)
+    .catch((err) => {
+      console.log("Error fetching categories:", err)
     })
-  }, [])
+  },[])
+
+  useEffect(() => {
+    getProducts()
+      .then((data) => {
+        if (data) {
+          const locationData = [
+            ...new Set(data.map((product) => product.location)),
+          ];
+          const locationObjects = locationData.map((location) => ({
+            id: location,
+            name: location,
+          }));
+
+          setProducts(data);
+          setIsLoading(false);
+          setLocations(locationObjects);
+        }
+      })
+      .catch((err) => {
+        setLoadingMessage(
+          `Unable to retrieve products. Status code ${err.message} on response.`
+        );
+      });
+  }, []);
 
   const searchProducts = (event) => {
-    getProducts(event).then(productsData => {
+    getProducts(event).then((productsData) => {
       if (productsData) {
-        setProducts(productsData)
+        setProducts(productsData);
       }
-    })
-  }
+    });
+  };
 
-  if (isLoading) return <p>{loadingMessage}</p>
+  if (isLoading) return <p>{loadingMessage}</p>;
+
+  //   return (
+  //     <>
+  //       <Filter
+  //         productCount={products.length}
+  //         onSearch={searchProducts}
+  //         locations={locations}
+  //       />
+
+  //       <div className="columns is-multiline">
+  //         {products.map((product) => (
+  //           <ProductCard product={product} key={product.id} />
+  //         ))}
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
-      <Filter productCount={products.length} onSearch={searchProducts} locations={locations} />
+      <Filter
+        productCount={products.length}
+        onSearch={searchProducts}
+        locations={locations}
+      />
 
       <div className="columns is-multiline">
-        {products.map(product => (
-          <ProductCard product={product} key={product.id} />
+        {categories.map((category) => (
+          <div key={category.name} className="column">
+            <h2>{category.name}</h2>
+            <div className="columns is multiline">
+              {products
+                .filter((product) => product.category === category.name)
+                .map((product) => (
+                  <ProductCard product={product} key={product.id} />
+                ))}
+            </div>
+          </div>
         ))}
       </div>
     </>
-  )
+  );
 }
 
 Products.getLayout = function getLayout(page) {
@@ -60,5 +107,5 @@ Products.getLayout = function getLayout(page) {
       <Navbar />
       {page}
     </Layout>
-  )
-}
+  );
+};
